b10d5fee1a681b82ad3723c27dacef8e7d63791d
daruiza <daruiza@gmail.com>
2025-02-23 10:58:37 -0500

    chnge espire jwt 2hours to 9hours

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -10,7 +10,7 @@ import { JwtModule } from '@nestjs/jwt';
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: {expiresIn: '7200s'}
+      signOptions: {expiresIn: '32400s'}
     })
   ],
   controllers: [AuthController],
