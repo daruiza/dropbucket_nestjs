adbf634bffa4b16d0bdb8f3e7756256d5db81b94
daruiza <daruiza@gmail.com>
2025-05-13 17:19:42 -0500

    configuración global utf8

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,12 @@ async function bootstrap() {
     credentials: true,
   });
 
+  // Configurar express para usar charset UTF-8
+  app.use((req, res, next) => {
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    next();
+  });
+
   // app.enableCors({
   //   origin: 'http://flutter:8080', // Ajusta esto según tu entorno
   //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
